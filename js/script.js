922242cd1b8eb820f60a28cab4561eedd1e8af47
Alexander091196 <91032455+Alexander091196@users.noreply.github.com>
2024-10-09 10:40:40 -0500

    response

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
-/* ------------- scroll section active link--------------------- */
+/* ------------- toggle icon navbar --------------------- */
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle('active');
+};
 
+/* ------------- scroll section active link --------------------- */
+let section = document.querySelectorAll("section");
+let navLink = document.querySelectorAll("header nav a");
 
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
+    id(top >= offset && top < offset + height);
+    {
+      navLink.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  /* ------------- sticky navbar --------------------- */
+  let header = document.querySelectorAll("header");
+
+  header.classList.toggle("sticky", window.screenY > 100);
+};
